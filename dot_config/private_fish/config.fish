8a3fa765be6ba4ef9e84dbf80222340b19b02ffc
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-05-13 14:23:55 +0530

    update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,7 +3,6 @@ end
 
 set -g fish_key_bindings fish_vi_key_bindings
 
-starship completions fish | source
 # Commands to run in interactive sessions can go here
 alias vim="nvim"
 alias v="nvim"
@@ -17,3 +16,18 @@ if test -f ~/.cargo/env.fish
 end
 
 source ~/.config/fish/env.fish
+
+# 
+# This takes a long time to load, so I have it in a separate file that I can source when needed
+#
+# source ~/.config/fish/env.tcin-aws.fish
+
+starship completions fish | source
+
+function reset_ssh_agent --description "Restart SSH agent and load the default Git key"
+    set -e SSH_AUTH_SOCK
+    set -e SSH_AGENT_PID
+
+    ssh-agent -c | sed 's/^setenv /set -gx /; s/;$//' | source
+    ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+end
