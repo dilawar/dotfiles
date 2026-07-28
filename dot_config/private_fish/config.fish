3cdf3b685e3ad4c824b0ee4caae7ea7485e26947
Dilawar Singh <dilawar.s@toyotaconnected.co.in>
2026-07-28 09:04:35 +0530

    Update .config/fish/config.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+end
+
+set -g fish_key_bindings fish_vi_key_bindings
+
+# Commands to run in interactive sessions can go here
+alias vim="nvim"
+alias v="nvim"
+set -Ux EDITOR nvim
+
+fish_add_path -g ~/.local/bin
+fish_add_path -g ~/.cargo/bin
+
+# 
+# This takes a long time to load, so I have it in a separate file that I can source when needed
+#
+# source ~/.config/fish/env.tcin-aws.fish
+
+starship completions fish | source
