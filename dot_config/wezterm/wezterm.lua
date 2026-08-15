576d61355fc84d18534563c2aa800b11fa08f993
Dilawar Singh <dilawar.s@toyotaconnected.co.in>
2026-08-15 21:27:19 +0530

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,7 +8,9 @@ local config = wezterm.config_builder()
 
 -- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
-config.initial_rows = 28
+config.initial_rows = 50
+
+config.font = wezterm.font("Fantasque Sans Mono")
 
 -- or, changing the font size and color scheme.
 config.font_size = 10
